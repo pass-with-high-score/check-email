63adacc891d15f988163764c234225f909927e94
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2025-02-16 12:46:18 +0700

    feat: update server binding to allow external access

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting the server at http://127.0.0.1:8080");
+    println!("Starting the server at http://0.0.0.0:8080");
     HttpServer::new(move || App::new().wrap(LogMiddleware).configure(routers::config))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }

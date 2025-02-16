c148d8dc3bdf324a75d3f11f62fc311f86dcd918
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2025-02-16 12:24:33 +0700

    feat: add startup log message for server initialization

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting the server at http://127.0.0.1:8080");
     HttpServer::new(move || App::new().wrap(LogMiddleware).configure(routers::config))
         .bind("127.0.0.1:8080")?
         .run()
